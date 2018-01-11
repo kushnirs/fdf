3eb2f0123f858e9fcff106a697702839a01515b5
Sergii KUSHNIR <skushnir@e2r13p6.unit.ua>
2018-01-11 21:34:59 +0200

    fix speed

--- draw_line.c
+++ draw_line.c
@@ -6,7 +6,7 @@
 /*   By: skushnir <skushnir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/05 21:58:17 by sergee            #+#    #+#             */
-/*   Updated: 2018/01/11 21:17:53 by skushnir         ###   ########.fr       */
+/*   Updated: 2018/01/11 21:34:55 by skushnir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,24 +73,25 @@ void	ft_draw_fdf(t_mlx *data)
 t_coord	ft_conversion_xyz(t_mlx *data, t_coord rot)
 {
 	t_coord	p[3];
+	double	func[6];
 
 	rot.x = rot.x * data->size + data->start.x;
 	rot.y = rot.y * data->size + data->start.y;
 	rot.z = rot.z * data->size / 10;
+	func[0] = cos(PI * data->rot.rx / 180);
+	func[1] = sin(PI * data->rot.rx / 180);
+	func[2] = cos(PI * data->rot.ry / 180);
+	func[3] = sin(PI * data->rot.ry / 180);
+	func[4] = cos(PI * data->rot.rz / 180);
+	func[5] = sin(PI * data->rot.rz / 180);
 	p[0].x = rot.x;
-	p[0].y = rot.y * cos(PI * data->rot.rx / 180) + rot.z *
-			sin(PI * data->rot.rx / 180);
-	p[0].z = rot.z * cos(PI * data->rot.rx / 180) - rot.y *
-			sin(PI * data->rot.rx / 180);
-	p[1].x = p[0].x * cos(PI * data->rot.ry / 180) + p[0].z *
-			sin(PI * data->rot.ry / 180);
+	p[0].y = rot.y * func[0] + rot.z * func[1];
+	p[0].z = rot.z * func[0] - rot.y * func[1];
+	p[1].x = p[0].x * func[2] + p[0].z * func[3];
 	p[1].y = p[0].y;
-	p[1].z = p[0].z * cos(PI * data->rot.ry / 180) - p[0].x *
-			sin(PI * data->rot.ry / 180);
-	p[2].x = WIDTH / 2 + p[1].x * cos(PI * data->rot.rz / 180) - p[1].y *
-			sin(PI * data->rot.rz / 180);
-	p[2].y = HIGH / 2 + p[1].x * sin(PI * data->rot.rz / 180) + p[1].y *
-			cos(PI * data->rot.rz / 180);
+	p[1].z = p[0].z * func[2] - p[0].x *func[3];
+	p[2].x = WIDTH / 2 + p[1].x * func[4] - p[1].y * func[5];
+	p[2].y = HIGH / 2 + p[1].x * func[5] + p[1].y * func[4];
 	p[2].z = p[1].z;
 	p[2].color = rot.color;
 	return (p[2]);
